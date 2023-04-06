7bad23047e73dcee8f1b28d8ffaa02506712fc82
Samuel Brashears <sbrashears@SamuelBs-MacBook-Pro.local>
2023-04-06 01:06:00 -0700

    rust?

---
+++ lua/custom/configs/misc.lua
@@ -0,0 +1,5 @@
+vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr = true})
+vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true})
+vim.keymap.set('v', '<leader>c', require('osc52').copy_visual)
+require('leap').add_default_mappings()
+vim.cmd(":TSInstall rust")
