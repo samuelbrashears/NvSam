e532a0314e855708712d8a62618dfa694c6dca11
Samuel Brashears <sbrashears@SamuelBs-MacBook-Pro.local>
2023-04-06 01:41:34 -0700

    finallyy

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -9,3 +9,19 @@
     filetypes = {"rust"},
     root_dir = lspconfig.util.root_pattern("Cargo.toml"),
   })
+
+  vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr = true})
+  vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true})
+  vim.keymap.set('v', '<leader>c', require('osc52').copy_visual)
+  require('leap').add_default_mappings()
+  -- Enable Rust parser in nvim-treesitter
+  require'nvim-treesitter.configs'.setup {
+    ensure_installed = {"rust"}, -- Install Rust parser
+    highlight = {
+      enable = true, -- Enable syntax highlighting
+    },
+    indent = {
+      enable = true, -- Enable indentation
+    },
+  }
+
