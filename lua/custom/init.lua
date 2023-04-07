1d3fafb0c6227e1d3d405a371e00a36e68c58505
Samuel Brashears <sbrashears@SamuelBs-MacBook-Pro.local>
2023-04-07 15:35:11 -0700

    quick fix

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,3 +1,3 @@
 vim.o.relativenumber = true
-vim.api.nvim_set_keymap('n', '<leader>yy', ':normal! ggVG<CR><leader> c', {noremap = false, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>yy', ':normal! ggVG<CR><leader>c<CR>', {noremap = false, silent = true})
 
