0230217a52f1c4ad25bcd3603f83e2b1e604a235
Samuel Brashears <sbrashears@SamuelBs-MacBook-Pro.local>
2023-04-18 11:02:59 -0700

    newest

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,18 +1,11 @@
-  local on_attach = require("plugins.configs.lspconfig").on_attach
-  local capabilities = require("plugins.configs.lspconfig").capabilities
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
 
-  local lspconfig = require "lspconfig"
+local lspconfig = require "lspconfig"
 
-  lspconfig.rust_analyzer.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = {"rust"},
-    root_dir = lspconfig.util.root_pattern("Cargo.toml"),
-  })
+vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr = true})
+vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true})
+vim.keymap.set('v', '<leader>c', require('osc52').copy_visual)
 
-  vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr = true})
-  vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true})
-  vim.keymap.set('v', '<leader>c', require('osc52').copy_visual)
+require('leap').add_default_mappings()
 
-  require('leap').add_default_mappings()
- 
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,5 +1,13 @@
 local plugins = {
   {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+      },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
       require "plugins.configs.lspconfig"
@@ -7,12 +15,26 @@ local plugins = {
     end,
   },
   {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "rust-analyzer",
-      },
-    },
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+      vim.g.rustfmt_options = '--edition 2021'
+    end
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require('rust-tools').setup(opts)
+    end
+  },
+  {
+    "mfussenegger/nvim-dap",
   },
   {
     "ggandor/leap.nvim",
